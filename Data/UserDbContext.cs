ae5bd5237523d41873f4f059e450102f870ed816
mayuri <mayurimbedare@gmail.com>
2023-09-14 18:56:58 +0530

    first commit

---
+++ Data/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using AuthSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AuthSystem.Data
+{
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<UserModel> User { get; set; }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthSystem.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+    }
+}
